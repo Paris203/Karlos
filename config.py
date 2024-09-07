388e72a77a9b80cf7409e5a0ac2bf830969316bd
Paris203 <53703377+Paris203@users.noreply.github.com>
2024-09-07 12:08:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ CUDA_VISIBLE_DEVICES = '0'  # The current version only supports one GPU training
 set = 'CUB'  # Different dataset with different
 model_name = ''
 
-batch_size = 8
+batch_size = 6
 vis_num = batch_size  # The number of visualized images in tensorboard
 eval_trainset = False  # Whether or not evaluate trainset
 save_interval = 1
