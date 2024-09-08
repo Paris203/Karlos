9af7be600b2ed6a2e543689aa87a0baacc501e6d
Paris203 <53703377+Paris203@users.noreply.github.com>
2024-09-08 08:36:29 +0800

    Update model.py

--- networks/model.py
+++ networks/model.py
@@ -173,7 +173,7 @@ class MainNet(nn.Module):
                                                 mode='bilinear', align_corners=True)  # [N, 3, 224, 224]
         #plot_and_save_image(local_imgs)
         local_fm, local_embeddings, _ = self.pretrained_model(local_imgs.detach())  # [N, 2048]
-        plot_and_save_image(local_fm)
+        plot_and_save_image(local_fm.detach())
         local_logits = self.rawcls_net(local_embeddings)  # [N, 200]
 
         proposalN_indices, proposalN_windows_scores, window_scores \
