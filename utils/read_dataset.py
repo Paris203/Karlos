b47d0048a3028ef40027e26b45b5756936b0bdcd
Paris203 <53703377+Paris203@users.noreply.github.com>
2024-09-08 19:45:22 -1200

    Update read_dataset.py

--- utils/read_dataset.py
+++ utils/read_dataset.py
@@ -7,11 +7,11 @@ def read_dataset(input_size, batch_size, root, set):
         print('Loading CUB trainset')
         trainset = dataset.CUB(input_size=input_size, root=root, is_train=True)
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                                  shuffle=True, num_workers=8, drop_last=False)
+                                                  shuffle=True, num_workers=4, drop_last=False)
         print('Loading CUB testset')
         testset = dataset.CUB(input_size=input_size, root=root, is_train=False)
         testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                                 shuffle=False, num_workers=8, drop_last=False)
+                                                 shuffle=False, num_workers=4, drop_last=False)
     elif set == 'CAR':
         print('Loading car trainset')
         trainset = dataset.STANFORD_CAR(input_size=input_size, root=root, is_train=True)
