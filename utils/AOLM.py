733a73a1b955f38a239d11b95e63dfb3e6b31544
Paris203 <53703377+Paris203@users.noreply.github.com>
2024-09-23 21:43:30 +0800

    Update AOLM.py

--- utils/AOLM.py
+++ utils/AOLM.py
@@ -80,7 +80,7 @@ def AOLM(fms, fm1):
         #print(f"m shape inside the loop :{m.shape}")
         mask_np = m.cpu().numpy().reshape(14, 14)
         fms_2 = fms_2.cpu().numpy().reshape(14, 14)
-        #plot_and_save_image(mask_np, save_path="saved_image.png")
+        plot_and_save_image(fms_2, save_path="saved_image.png")
         component_labels = measure.label(mask_np)
         #print(f"component_labels shape {component_labels.shape}")
 
