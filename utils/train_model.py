bccb0635f1354c3b3546239fb038b343511a39bf
Paris203 <53703377+Paris203@users.noreply.github.com>
2024-09-29 09:42:10 +0800

    Update train_model.py

--- utils/train_model.py
+++ utils/train_model.py
@@ -8,10 +8,10 @@ from config import max_checkpoint_num, proposalN, eval_trainset, set
 from utils.eval_model import eval
 from utils.AOLM import AOLM
 from config import pretrain_path
-from networks import resnet
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-pretrain_model = resnet.resnet50(pretrained=True, pth_path=pretrain_path).to(device)
+
 
 
 def load_checkpoint(checkpoint_path, model):
@@ -140,14 +140,13 @@ def train(model,
 
             # Get the model outputs, including fm and conv5_b
             proposalN_windows_score, proposalN_windows_logits, indices, \
-            window_scores, _, raw_logits, local_logits, _ = model(images, epoch, i, 'train')
+            window_scores, _, raw_logits, local_logits, local_imgs, local_fm, conv5_b = model(images, epoch, i, 'train')
 
             # Assuming fm is the feature map and conv5_b is also a feature map output
-            fm, embedding, conv5_b = pretrain_model(images.cpu())
-            print(f" shape fm in model {fm.shape}, conv5_b shape {conv5_b.shape}")
+            print(f" shape fm in model {local_fm.shape}, conv5_b shape {conv5_b.shape}")
 
             # Call AOLM function to get the coordinates and lam
-            coordinates, lam = AOLM(fm.detach(), conv5_b.detach())
+            coordinates, lam = AOLM(local_fm.detach(), conv5_b.detach())
             print("lamda value", lam)
 
             raw_loss = criterion(raw_logits, labels)
